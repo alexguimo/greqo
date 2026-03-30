235adc7a864316fba0330cdaffb22dbcdb97422e
alexguimo <95321752+alexguimo@users.noreply.github.com>
2026-03-29 22:39:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
+import os
 
+app = FastAPI()
+
+API_TOKEN = os.getenv("API_TOKEN")
+
+class Comando(BaseModel):
+    texto: str
+
+@app.get("/")
+def home():
+    return {"mensaje": "Greqo activo 🚀"}
+
+@app.post("/comando")
+def procesar(data: Comando, authorization: str = Header(None)):
+
+    if authorization != f"Bearer {API_TOKEN}":
+        raise HTTPException(status_code=401, detail="No autorizado")
+
+    texto = data.texto.lower()
+
+    if "llama" in texto:
+        return {"accion": "llamar", "contacto": "mamá"}
+
+    if "abre whatsapp" in texto:
+        return {"accion": "abrir_app", "app": "com.whatsapp"}
+
+    return {
+        "accion": "hablar",
+        "respuesta": "No entendí, repite por favor."
+    }
