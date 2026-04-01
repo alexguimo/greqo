144ad16df26541d8ee35af156310a2eca0483983
alexguimo <95321752+alexguimo@users.noreply.github.com>
2026-03-31 22:28:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ import os
 import random
 
 app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # luego lo hacemos más seguro
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 security = HTTPBearer()
 
 API_TOKEN = os.getenv("API_TOKEN")
